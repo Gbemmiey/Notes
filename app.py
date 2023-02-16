2b4bb9d1f769cfaae3b3d78095e61b74b25aa0fb
Gbemmiey <gbemfemalabi@gmail.com>
2023-02-16 15:25:23 +0100

    Update: modified config.py

--- app.py
+++ app.py
@@ -2,75 +2,67 @@ import json
 from flask import Flask, render_template, redirect, jsonify, abort
 
 import models
+from config import Config
 
 
-app = Flask(__name__)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
+    @app.route('/')
+    @app.route('/logout')
+    def homepage():
+        """Method to display homepage"""
+        return render_template('views/index.html')
 
-@app.route('/')
-@app.route('/logout')
-def homepage():
-    """Method to display homepage"""
-    return render_template('views/index.html')
+    @app.route('/login')
+    def show_login():  # put application's code here
+        """Method to display login form"""
+        return render_template('forms/login.html')
 
+    @app.route('/signup')
+    def display_signup():  # put application's code here
+        """Method to display signup form"""
+        return render_template('forms/signup.html')
 
-@app.route('/login')
-def show_login():  # put application's code here
-    """Method to display login form"""
-    return render_template('forms/login.html')
+    @app.route('/dashboard')
+    def display_dashboard():
+        """Method to display a user's dashboard"""
+        return render_template('views/dashboard.html')
 
+    @app.route('/note')
+    def display_note():
+        """Method to display a user's dashboard"""
+        return render_template('views/note.html')
 
-@app.route('/signup')
-def display_signup():  # put application's code here
-    """Method to display signup form"""
-    return render_template('forms/signup.html')
+    @app.route('/login_user')
+    def login_user():
+        return render_template('views/dashboard.html')
 
+    @app.route('/create_user')
+    def create_user():
+        return redirect('/')
 
-@app.route('/dashboard')
-def display_dashboard():
-    """Method to display a user's dashboard"""
-    return render_template('views/dashboard.html')
+    @app.errorhandler(404)
+    def not_found(error):
+        """
+        Error handler for 404
+        """
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Not Found'
+        }), 404
 
+    @app.errorhandler(422)
+    def unprocessable_entity(error):
+        """
+        Error handler for 422
+        """
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'Unprocessable Entity'
+        }), 422
 
-@app.route('/note')
-def display_note():
-    """Method to display a user's dashboard"""
-    return render_template('views/note.html')
-
-
-@app.route('/login_user')
-def login_user():
-    return render_template('views/dashboard.html')
-
-
-@app.route('/create_user')
-def create_user():
-    return redirect(homepage)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """
-    Error handler for 404
-    """
-    return jsonify({
-        'success': False,
-        'error': 404,
-        'message': 'Not Found'
-    }), 404
-
-
-@app.errorhandler(422)
-def unprocessable_entity(error):
-    """
-    Error handler for 422
-    """
-    return jsonify({
-        'success': False,
-        'error': 422,
-        'message': 'Unprocessable Entity'
-    }), 422
-
-
-if __name__ == '__main__':
-    app.run()
+    return app
--- config.py
+++ config.py
@@ -1,21 +1,31 @@
 import os
 
-from dotenv import dotenv_values
+from dotenv import dotenv_values, load_dotenv
 
-env_variables = dotenv_values()
-
-database_name = env_variables['DATABASE_NAME']
-username = env_variables['DATABASE_USERNAME']
-password = env_variables['DATABASE_PASSWORD']
-database_uri = env_variables['DATABASE_URI']
+try:
+    env_variables = load_dotenv()
+    database_name = env_variables['DATABASE_NAME']
+    username = env_variables['DATABASE_USERNAME']
+    password = env_variables['DATABASE_PASSWORD']
+    database_uri = env_variables['DATABASE_URI']
+except ValueError:
+    env_variables = dotenv_values()
+    database_name = env_variables['DATABASE_NAME']
+    username = env_variables['DATABASE_USERNAME']
+    password = env_variables['DATABASE_PASSWORD']
+    database_uri = env_variables['DATABASE_URI']
 
 database_path = f'postgresql://{username}:{password}@{database_uri}/{database_name}'
-SQLALCHEMY_DATABASE_URI = database_path
-SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
+class Config:
+    """Class to contain sensitive data"""
+    SQLALCHEMY_DATABASE_URI = database_path
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    SECRET_KEY = os.urandom(32)
+    # Grabs the folder where the script runs.
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    # Enable debug mode.
+    DEBUG = True
