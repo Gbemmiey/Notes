991a3ae1a355993bb83b57beb0cbe250648f1c11
Gbemmiey <gbemfemalabi@gmail.com>
2023-02-16 16:03:29 +0100

    Update: Modified homepage.css, index.html & requirements.txt

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, redirect, jsonify, abort
 import models
 from config import Config
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
+
 
 @app.route('/')
 @app.route('/logout')
@@ -14,33 +14,40 @@ def homepage():
     """Method to display homepage"""
     return render_template('views/index.html')
 
+
 @app.route('/login')
 def show_login():  # put application's code here
     """Method to display login form"""
     return render_template('forms/login.html')
+
 
 @app.route('/signup')
 def display_signup():  # put application's code here
     """Method to display signup form"""
     return render_template('forms/signup.html')
 
+
 @app.route('/dashboard')
 def display_dashboard():
     """Method to display a user's dashboard"""
     return render_template('views/dashboard.html')
+
 
 @app.route('/note')
 def display_note():
     """Method to display a user's dashboard"""
     return render_template('views/note.html')
 
+
 @app.route('/login_user')
 def login_user():
     return render_template('views/dashboard.html')
 
+
 @app.route('/create_user')
 def create_user():
     return redirect('/')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -52,6 +59,7 @@ def not_found(error):
         'error': 404,
         'message': 'Not Found'
     }), 404
+
 
 @app.errorhandler(422)
 def unprocessable_entity(error):
@@ -65,5 +73,5 @@ def unprocessable_entity(error):
     }), 422
 
 
-if __name__=='main':
+if __name__ == 'main':
     app.run()
