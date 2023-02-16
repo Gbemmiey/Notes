8ccf0e68a01f8f619dc19524af28b99827706243
Gbemmiey <gbemfemalabi@gmail.com>
2023-02-16 23:28:12 +0100

    Update: Added functionality for logging in & signing up

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
 import json
-from flask import Flask, render_template, redirect, jsonify, abort
+from flask import Flask, render_template, redirect
+from flask import request, jsonify, abort, url_for
+from flask import flash
 
-import models
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
+from models import User, Note, setup_db
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+with app.app_context():
+    setup_db(app)
+    # print(app.app_context())
+
 
 @app.route('/')
-@app.route('/logout')
 def homepage():
     """Method to display homepage"""
     return render_template('views/index.html')
 
 
 @app.route('/login')
-def show_login():  # put application's code here
+def display_login():  # put application's code here
     """Method to display login form"""
-    return render_template('forms/login.html')
+    error = None
+    return render_template('forms/login.html', error=error)
 
 
 @app.route('/signup')
 def display_signup():  # put application's code here
     """Method to display signup form"""
-    return render_template('forms/signup.html')
+    error = None
+    return render_template('forms/signup.html', error=error)
 
 
 @app.route('/dashboard')
@@ -39,14 +49,52 @@ def display_note():
     return render_template('views/note.html')
 
 
-@app.route('/login_user')
+@app.route('/login_user', methods=['POST'])
 def login_user():
-    return render_template('views/dashboard.html')
+    error = f'Invalid credentials'
+
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        user = User.query.filter_by(email=email).one()
+
+        if check_password_hash(user.password, password):
+            flash("Logged In")
+            return redirect(url_for('display_dashboard'))
+        else:
+            flash("Invalid credentials")
+            return render_template('forms/login.html', error=error)
+
+    except NoResultFound:
+        return render_template('forms/login.html', error=error)
 
 
-@app.route('/create_user')
+@app.route('/create_user', methods=['POST'])
 def create_user():
-    return redirect('/')
+    email = request.form['email']
+    password = request.form['password']
+    username = request.form['username']
+
+    hashed_pass = generate_password_hash(password)
+    try:
+        user = User(name=username, email=email, password=hashed_pass)
+        user.insert()
+        flash("Account successfully created")
+        return redirect(url_for('show_login'))
+    except IntegrityError:
+        user.rollback()
+        error = f'Email already exists'
+
+    return render_template('forms/signup.html', error=error)
+
+    # except Exception as e:
+    #     print(repr(e))
+
+
+@app.route('/logout')
+def logout_user():
+    return redirect(url_for('homepage'))
 
 
 @app.errorhandler(404)
--- models.py
+++ models.py
@@ -1,3 +1,56 @@
-# from flask-sqlalchemy import sqlalchemy
-# db = sqlalchemy()
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
+
+from config import Config
+
+db = SQLAlchemy()
+Base = declarative_base()
+
+
+def setup_db(app, database_path=Config.SQLALCHEMY_DATABASE_URI):
+    """
+    setup_db(app)
+        binds a flask application and a SQLAlchemy service
+    """
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    # db.drop_all()
+    db.create_all()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))
+    email = Column(String(64), unique=True)
+    password = Column(String)
+    notes = relationship('Note')
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def rollback(self):
+        db.session.rollback()
+
+
+class Note(db.Model):
+    __tablename__ = 'note'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    text = Column(String)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    created_on = Column(DateTime)
+    modified_on = Column(DateTime)
