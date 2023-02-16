9815602cbc04baf9f75e98584a82ba02906f9fc8
Gbemmiey <gbemfemalabi@gmail.com>
2023-02-16 11:34:45 +0100

    Update: initialized html pages and endpoints in app.py

--- app.py
+++ app.py
@@ -1,11 +1,61 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, redirect, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def homepage():  # put application's code here
+    return render_template('views/index.html')
+
+
+@app.route('/login')
+def show_login():  # put application's code here
+    return render_template('views/index.html')
+
+
+@app.route('/signup')
+def display_signup():  # put application's code here
+    return render_template('views/index.html')
+
+
+@app.route('/')
+def homepage():  # put application's code here
+    return render_template('views/index.html')
+
+
+@app.route('/login_user')
+def login_user():
+    return render_template('views/dashboard.html')
+
+
+@app.route('/create_user')
+def create_user():
+    return redirect(homepage)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Error handler for 404
+    """
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Not Found'
+    }), 404
+
+
+@app.errorhandler(422)
+def unprocessable_entity(error):
+    """
+    Error handler for 422
+    """
+    return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'Unprocessable Entity'
+    }), 422
 
 
 if __name__ == '__main__':
